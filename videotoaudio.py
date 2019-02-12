6c9fdd7ee7200505a9512338a171c27781398811
Arun kumar s <arunkumar98.ak95@gmail.com>
2019-02-12 11:48:47 +0530

    py file that can convert video to audio and then convert the audio to text

---
+++ videotoaudio.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+import moviepy.editor as mp
+clip = mp.VideoFileClip("testvid.mp4").subclip(0,60)
+clip.audio.write_audiofile("theaudio.wav")
+r = sr.Recognizer()
+audio ='theaudio.wav'
+with sr.AudioFile(audio) as source:
+    print('Speak anything:')
+    audio = r.record(source)
+    print('Done')
+try:
+       text = r.recognize_google(audio)
+       print(text)
+except Exception as e:
+        print(e)
